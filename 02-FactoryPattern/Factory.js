873d4972abd6156cf50647c400764287399c22c1
ajay <ap221882@gmail.com>
2023-07-25 21:44:17 +0530

    initial solution

--- 02-FactoryPattern/Factory.js
+++ 02-FactoryPattern/Factory.js
@@ -17,27 +17,42 @@ class Laptop extends Computer {
   }
 }
 
-class Order {
+class Factory {
+  getComputer(type) {
+    const factoryMapper = {
+      PC: new PC(),
+      Laptop: new Laptop(),
+    };
+    return factoryMapper?.[type];
+  }
+}
 
+class Order {
   placeOrder(type, cpu, ram, storage, display) {
     let product;
 
-    switch (type) {
-      case "PC":
-        product = new PC();
-        product.makeComputer(cpu, ram, storage, display);
-        return product;
-      case "Laptop":
-        product = new Laptop();
-        product.makeComputer(cpu, ram, storage, display);
-        return product;
-    }
-    return null;
+    const computerFactory = new Factory();
+    //~! removed switch case
+    // switch (type) {
+    //   case "PC":
+    //     product = new PC();
+    //     product.makeComputer(cpu, ram, storage, display);
+    //     return product;
+    //   case "Laptop":
+    //     product = new Laptop();
+    //     product.makeComputer(cpu, ram, storage, display);
+    //     return product;
+    // }
+
+    product = computerFactory.getComputer(type);
+    product?.makeComputer(type, cpu, ram, storage, display);
+
+    return product ? product : null;
   }
 }
 
 const order = new Order();
+
 computer = order.placeOrder("PC", 4, 16, 512, 16);
+
 console.log(computer.getInfo());
-
-
